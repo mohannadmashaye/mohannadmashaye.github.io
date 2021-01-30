f8f08dabeb7949503cd849eaef0506e0b2b5348d
mohannadmashaye <59237882+mohannadmashaye@users.noreply.github.com>
2021-01-30 03:28:26 -0500

    Update site.js

--- js files/site.js
+++ js files/site.js
@@ -1,20 +1,11 @@
-//All the sites javascript will be written in this files
-
+//Function(s) to randomize greetings on the home page
 function titleTranslation(){
 
-  var translations = ["Welcome!", "Willkommen!","¡Bienvenido!", "Bienvenue!", "Benvenuto!","Hoş geldin!", "Добро пожаловать!", "Kuwakaribisha!","Soo dhawow!", "እንኳን ደህና መጣህ!", "환영합니다!", "!أهلا بك"];
-  var translationsLength = translations.length;
-
-  var translationsRandom =  translations[Math.floor(Math.random() * translationsLength)];
-  var title;
-  var counter=0;
-  for (var i = 0; i < translationsLength; i++) {
-    if(translations[i] != translationsRandom){
-    document.getElementById('maintitle').innerHTML = translationsRandom;
-    }
-  }
+  let translations = ["Welcome!", "Willkommen!","¡Bienvenido!", "Bienvenue!", "Benvenuto!","Hoş geldin!", "Добро пожаловать!", "Kuwakaribisha!","Soo dhawow!", "እንኳን ደህና መጣህ!", "환영합니다!", "!أهلا بك"];
+  let translationsRandom =  translations[Math.floor(Math.random() * translations.length)];
+  return document.getElementById('maintitle').innerHTML = translationsRandom;
 }
 
 function timer(){
-  var repeatTitle = setInterval(titleTranslation, 4000);
+  let repeatTitle = setInterval(titleTranslation, 5000);
 }
